09166cb1fde902e08c8babf88d7d1752bfc6fa4f
kassym <kassym@hexsletcourse.mshome.net>
2024-11-20 07:00:02 +0000

    Change brain_games add fun welcome

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+
 def greet(text):
     print(text)
 
 def main():
     greet("Welcome to the Brain Games!")
+    welcome_user()
 
 if __name__ == '__main__':
     main()
+    #welcome_user()
