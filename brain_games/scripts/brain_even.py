8b73256123ad31c53b3665d7977878c6e4f0bbc1
kazakalban <kasymsauyt@gmail.com>
2024-11-21 07:39:24 +0000

    Test brain-even to succsec

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,12 @@
+from brain_games.scripts.brain_games import main as hello_main 
+
 def game_parity_check():
-    print('Test function game_parity_check')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    
 
 
 def main():
+    hello_main()
     game_parity_check()
 
 
