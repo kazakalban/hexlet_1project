53d05b51fb1535d8827533e739470ca78a5d9f2d
kazakalban <kasymsauyt@gmail.com>
2024-11-25 09:08:46 +0000

    Add function to calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,38 @@
+from brain_games.cli import welcome_user as main_user_name
+from random import randint
+import prompt
+
+def game_calc():
+    user_name = main_user_name()
+    print('What is the result of the expression?')
+    correct_answer = 0
+    while correct_answer != 3:
+        number_a = randint(1, 25)
+        number_b = randint(1, 25)
+        operation_symbol = ['-', '*', '+']
+        operation = randint(0,2)
+        print(f'Question {number_a} {operation_symbol[operation]} {number_b}')
+        if operation_symbol[operation] == '+':
+            correct_result = number_a + number_b
+        elif operation_symbol[operation] == '-':
+            correct_result = number_a - number_b
+        elif operation_symbol[operation] == '*':
+            correct_result = number_a * number_b
+
+        user_answer = prompt.integer('Your answer: ')
+        if user_answer == correct_result:
+            print('Correct!')
+            correct_answer += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_result}'"
+                  f"Let's try again, {user_name}!")
+            break
+        if correct_answer == 3:
+            print(f'Congratulations, {user_name}!')
+
+def main():
+    game_calc()
+
+
+if __name__ == "__main__":
+    main()
