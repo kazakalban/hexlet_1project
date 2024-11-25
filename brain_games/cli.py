fa4ce752e13978dd412dc6ad289413fd0ccef71e
kazakalban <kasymsauyt@gmail.com>
2024-11-25 09:41:26 +0000

    Welcome text moved to welcome_user

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,7 @@ import prompt
 
 
 def welcome_user():
+    print("Welcome to the Brain Games!")
     ask_text = 'May I have your name? '
     name = prompt.string(ask_text)
     print(f'Hello, {name}!')
