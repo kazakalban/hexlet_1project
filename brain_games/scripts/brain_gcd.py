d6c8d4192db0b57906e871564474687ccce0fc23
kazakalban <kasymsauyt@gmail.com>
2024-11-29 12:14:51 +0000

    Correct linter

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -4,7 +4,6 @@ import math
 import prompt  # Внешние библиотеки
 
 from brain_games.cli import welcome_user as main_user_name  # Локальные модули
-
 from brain_games.scripts.brain_games import max_raund_game as max_raund
 
 
